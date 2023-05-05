b5071d703f90a72f6d0b50595b19685baca47b29
Sychov-Yurii <sychov.yurii@outlook.com>
2023-05-05 22:56:58 +0200

    work 3

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ console.log(pricePerItem); // +1500
 
 // ================= work 3 ================= //
 
+let topSpeed = 160
+let distance = 617.54
+let login = "mango935"
+let isOnline = true
+let isAdmin = false
+
+// ================= work 4 ================= //
+
