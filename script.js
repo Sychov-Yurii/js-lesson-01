ed72b0230096d62bdb776ecc301d0e0999b3e348
Sychov-Yurii <sychov.yurii@outlook.com>
2023-05-05 22:52:18 +0200

    work 2

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-// work 1 //
+// ================= work 1 ================= //
 
-const productName = "Droid";
-const pricePerItem = 2000;
+// const productName = "Droid";
+// const pricePerItem = 2000;
+
+// ================= work 2 ================= //
+
+let productName = "Droid";
+productName = "Repair droid"
+console.log(productName); // Repair droid
+
+let pricePerItem = 2000;
+pricePerItem = pricePerItem + 1500
+console.log(pricePerItem); // +1500
+
+// ================= work 3 ================= //
+
