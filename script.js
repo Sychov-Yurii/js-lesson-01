7d36c642365a6c71fdd1cbe9be70a3c127e7ec75
Sychov-Yurii <sychov.yurii@outlook.com>
2023-05-07 12:35:54 +0200

    add text

--- script.js
+++ script.js
@@ -15,11 +15,33 @@
 
 // ================= work 3 ================= //
 
-let topSpeed = 160
-let distance = 617.54
-let login = "mango935"
-let isOnline = true
-let isAdmin = false
+// let topSpeed = 160
+// let distance = 617.54
+// let login = "mango935"
+// let isOnline = true
+// let isAdmin = false
 
 // ================= work 4 ================= //
 
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+
+// ================= work 5 ================= //
+hsr
+
+
+
+
+//Створіть функцію endsWith(), яка порівнює підрядок str1 із закінченням вихідного рядка
+//str і визначає чи закінчується рядок символами підрядка.
+
+// const str = "Кожен мисливець бажає знати";
+// const str1 = "скрипт";
+// const str2 = "знати";
+
+function endsWith(str, substring) {}
+
+    return str.endsWith(substring)
+console.log(endsWith(str, str1)); // false
+console.log(endsWith(str, str2)); // true
